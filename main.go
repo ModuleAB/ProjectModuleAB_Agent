d3d6c56b1c3df98715e3254477a82f10bd5737bb
TonyChyi <tonychee1989@gmail.com>
2016-05-23 10:06:39 +0800

    fix

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	logger.AppLog.Info("ModuleAB agent", common.Version, "starting...")
+	logger.AppLog.Info("ModuleAB agent", version.Version, "starting...")
 	logger.AppLog.Level = logger.StringLevelToInt(
 		conf.AppConfig.GetString("loglevel"),
 	)
