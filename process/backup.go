dea706c5785244e681656b958b8f3630110dbf29
TonyChyi <tonychee1989@gmail.com>
2016-06-21 08:34:00 +0800

    parell

--- process/backup.go
+++ process/backup.go
@@ -90,95 +90,107 @@ func (b *BackupManager) Run(h *models.Hosts) {
 					continue
 				}
 				if strings.HasPrefix(filename, v.Path) {
-					if v.BackupSet == nil {
-						logger.AppLog.Warn("No BackupSet or AppSet got, skip")
-						continue
-					}
-					_, file := path.Split(filename)
-					record := &models.Records{
-						Filename:   file,
-						Host:       h,
-						BackupSet:  v.BackupSet,
-						AppSet:     h.AppSet,
-						Path:       v,
-						Type:       models.RecordTypeBackup,
-						BackupTime: time.Now(),
-					}
-					logger.AppLog.Debug("Now is:", time.Now().Format(time.RFC3339))
-					logger.AppLog.Debug("Record:", record)
-					ossclient, err := oss.New(
-						v.BackupSet.Oss.Endpoint, b.ApiKey, b.ApiSecret)
-					if err != nil {
-						logger.AppLog.Warn("Error while connect to oss:", err)
-						fmt.Println("Error while connect to oss:", err)
-						continue
-					}
-					bucket, err := ossclient.Bucket(v.BackupSet.Oss.BucketName)
-					if err != nil {
-						logger.AppLog.Warn(
-							"Error while retrievaling bucket:", err)
-						fmt.Println(
-							"Error while retrievaling bucket:", err)
-						continue
-					}
-
-					ps := strings.Split(path.Dir(record.GetFullPath()), "/")
-					var (
-						dir           string
-						lock          sync.Mutex
-						dirCreated    = true
-						isRecoverFile = false
-					)
-					lock.Lock()
-					logger.AppLog.Debug("recoverdFiles is:", recoverdFiles)
-					for k, v := range recoverdFiles {
-						if event.Name == v {
-							recoverdFiles = append(recoverdFiles[:k], recoverdFiles[k+1:]...)
-							isRecoverFile = true
-						}
-					}
-					lock.Unlock()
-					if isRecoverFile {
-						logger.AppLog.Info("File", event.Name, "is recover file.")
-						continue
-					}
-					for _, p := range ps {
-						dir = fmt.Sprintf("%s%s/", dir, p)
-						err = bucket.PutObject(dir, strings.NewReader(""))
-						if err != nil {
-							logger.AppLog.Warn(
-								"Error while making dir on bucket:", err)
-							fmt.Println(
-								"Error while making dir on bucket:", err)
-							dirCreated = false
-							break
-						}
-					}
-					if !dirCreated {
-						continue
-					}
-					err = bucket.PutObjectFromFile(
-						record.GetFullPath(),
-						event.Name,
-						oss.Routines(common.UploadThreads),
-					)
-					if err != nil {
-						logger.AppLog.Warn(
-							"Error while uploading:", err)
-						fmt.Println(
-							"Error while uploading:", err)
-						continue
-					}
-					err = client.UploadRecord(record)
-					if err != nil {
-						logger.AppLog.Warn(
-							"Error while recording:", err)
-						fmt.Println(
-							"Error while recording:", err)
-						continue
-					}
+					go b.doBackup(v, filename, h, event.Name)
 				}
 			}
 		}
 	}
+}
+
+func (b *BackupManager) doBackup(v *models.Paths, filename string, h *models.Hosts, eName string) {
+	if v.BackupSet == nil {
+		logger.AppLog.Warn("No BackupSet or AppSet got, skip")
+		return
+	}
+	_, file := path.Split(filename)
+	record := &models.Records{
+		Filename:   file,
+		Host:       h,
+		BackupSet:  v.BackupSet,
+		AppSet:     h.AppSet,
+		Path:       v,
+		Type:       models.RecordTypeBackup,
+		BackupTime: time.Now(),
+	}
+	logger.AppLog.Debug("Now is:", time.Now().Format(time.RFC3339))
+	logger.AppLog.Debug("Record:", record)
+	ossclient, err := oss.New(
+		v.BackupSet.Oss.Endpoint, b.ApiKey, b.ApiSecret)
+	if err != nil {
+		logger.AppLog.Warn("Error while connect to oss:", err)
+		fmt.Println("Error while connect to oss:", err)
+		return
+	}
+	bucket, err := ossclient.Bucket(v.BackupSet.Oss.BucketName)
+	if err != nil {
+		logger.AppLog.Warn(
+			"Error while retrievaling bucket:", err)
+		fmt.Println(
+			"Error while retrievaling bucket:", err)
+		return
+	}
+
+	ps := strings.Split(path.Dir(record.GetFullPath()), "/")
+	var (
+		dir           string
+		lock          sync.Mutex
+		dirCreated    = true
+		isRecoverFile = false
+	)
+	lock.Lock()
+	logger.AppLog.Debug("recoverdFiles is:", recoverdFiles)
+	for k, v := range recoverdFiles {
+		if eName == v {
+			recoverdFiles = append(recoverdFiles[:k], recoverdFiles[k+1:]...)
+			isRecoverFile = true
+		}
+	}
+	lock.Unlock()
+	if isRecoverFile {
+		logger.AppLog.Info("File", eName, "is recover file.")
+		return
+	}
+	for delay := 0; delay <= 50; delay += 10 {
+		if delay > 0 {
+			logger.AppLog.Info("Retry in", delay, "Minutes")
+		}
+		time.Sleep(time.Duration(delay) * time.Minute)
+		for _, p := range ps {
+			dir = fmt.Sprintf("%s%s/", dir, p)
+			err = bucket.PutObject(dir, strings.NewReader(""))
+			if err != nil {
+				logger.AppLog.Warn(
+					"Error while making dir on bucket:", err)
+				fmt.Println(
+					"Error while making dir on bucket:", err)
+				dirCreated = false
+				break
+			}
+		}
+		if !dirCreated {
+			continue
+		}
+		err = bucket.PutObjectFromFile(
+			record.GetFullPath(),
+			eName,
+			oss.Routines(common.UploadThreads),
+		)
+		if err != nil {
+			logger.AppLog.Warn(
+				"Error while uploading:", err)
+			fmt.Println(
+				"Error while uploading:", err)
+			continue
+		}
+		err = client.UploadRecord(record)
+		if err != nil {
+			logger.AppLog.Warn(
+				"Error while recording:", err)
+			fmt.Println(
+				"Error while recording:", err)
+			continue
+		}
+		return
+	}
+	logger.AppLog.Warn("Backup file:", eName, "Failed.")
 }
